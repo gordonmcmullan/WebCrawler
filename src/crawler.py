266f8c66fae95bd7072ddd49bcb0044dcf5e6c21
Gordon McMullan <gordon.mcmullan@digital.hmrc.gov.uk>
2017-05-18 16:36:49 +0100

    Changed default crawl start

--- src/crawler.py
+++ src/crawler.py
@@ -8,14 +8,14 @@ parser = argparse.ArgumentParser(description="Crawl Web Pages")
 parser.add_argument('--target',
                     type=str,
                     nargs='?',
-                    default="https://gocardless.com/",
-                    help='Start point for the crawl default is https://gocardless.com/')
+                    default="https://google.com/",
+                    help='Start point for the crawl default is https://google.com/')
 
 parser.add_argument('--logging_level',
                     type=str,
                     nargs='?',
                     default=logging.ERROR,
-                    help='Start point for the crawl default is https://gocardless.com/')
+                    help='Logging level default is error')
 
 args = parser.parse_args()
 logging.debug("Starting a webcrawl at %s" %args.target)
